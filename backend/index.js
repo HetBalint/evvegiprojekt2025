2d1514b5238050ba5726fbdb9e56e102c22e111b
HetBalint <hetenyibalint06@gmail.com>
2025-01-22 22:15:14 +0100

    01.22 napvege

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express"
+import mysql from "mysql"
+import cors from "cors"
 
 const app = express()
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send("A szerver fut")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
- 
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
